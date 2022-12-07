5432094daed96851fb5fb28dad7a38304f525fe8
Ma. Eden Ramoneda <edenlee143@gmail.com>
2022-12-07 11:49:36 +0800

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
   <v-app id="inspire">
-    <v-main>
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
       <v-container>
-        <v-fade-transition mode="out-in">
-          <router-view></router-view>
-        </v-fade-transition>
+
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <router-view></router-view>
+            </v-sheet>
       </v-container>
     </v-main>
-    <!-- <v-footer color="black" app inset padless absolute>
-			<v-container class="white--text text-center">
-        Copyright © 2020 OTis Inc. All Rights Reserved
-			</v-container>       
-			<v-spacer></v-spacer>
-		</v-footer> -->
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
   
     mounted() {
      //this.$fcm.initPush();
