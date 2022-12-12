a20ff5e86602c23e880aa3fb095fbef674a43774
eerikabondoc <erikaritzelle@gmail.com>
2022-12-12 14:24:43 +0800

    Added material design icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
 import vuetify from './plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
 new Vue({
   router,
