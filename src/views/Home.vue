a98666066303d8013052936f1c28e48bcb942125
eerikabondoc <erikaritzelle@gmail.com>
2022-12-12 12:53:24 +0800

    Download JSON file format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <h2>Upload JSON File</h2>
+        <h2>JSONファイルのインポート</h2>
       </v-col>
       <v-col cols="12">
           <v-divider></v-divider>
@@ -10,7 +10,6 @@
     </v-row>
     <v-row>
       <v-col cols="6">
-        <!-- <input type="file" @change="loadTextFromFile" /> -->
         <v-file-input
           id="selectFiles"
           label="File input"
@@ -19,16 +18,6 @@
           prepend-icon="mdi-paperclip"
           @change="loadTextFromFile"
         ></v-file-input>
-      </v-col>
-      <v-col cols="6">
-        <v-btn
-          id="import"
-          class="white--text"
-          color="black"
-          rounded
-        >
-          Submit
-        </v-btn>
       </v-col>
       <v-col cols="6">
         <pre id="result"></pre>
@@ -42,64 +31,89 @@
           <v-divider></v-divider>
       </v-col>
     </v-row>
-    <v-row v-for="(value, key) in users" v-bind:key="key">
-        <v-col cols="4">
-          <v-subheader>{{value.key}}</v-subheader>
-        </v-col>
-        <v-col cols="8">
-          <!-- <input :type="text" :name="key" :v-model="test" :value="value" /> -->
-          <v-text-field
-            v-model="users[key].value"
-            :value="value.value"
-            :name="value.key"
-            outlined
-          ></v-text-field>
-        </v-col>
-        <!-- <input :v-model="value" type="text" :value="value"/> -->
+    <v-row>
+      <v-col cols="12">
+        <v-btn
+          @click="getNewJSONValue"
+          :key="uploadFile"
+          class="white--text"
+          color="black"
+          rounded
+        >
+          Export
+        </v-btn>
+        <!-- {{data}} -->
+      </v-col>
     </v-row>
-    <v-btn
-      @click="testGet"
-      :key="uploadFile"
-    >
-      Submit
-    </v-btn>
-    {{users}}
+    <!-- <div id="element">
+      <div v-if="!data.length">Invalid</div> -->
+      <v-row v-for="(value, key) in data" v-bind:key="key">
+          <v-col cols="4">
+            <v-subheader>{{value.key}}</v-subheader>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field
+              v-model="data[key].value"
+              :value="value.value"
+              :name="value.key"
+              outlined
+            ></v-text-field>
+          </v-col>
+      </v-row>
+    <!-- </div> -->
   </v-container>
 </template>
 
 <script>
+import downloadjs from 'downloadjs';
+
 export default {
   data() {
     return {
       uploadFile: null,
-      users: [],
+      data: [],
       result: null,
       selectFiles: null,
-      value: null,
+      value: null
     };
   },
   methods:{
     loadTextFromFile() {
+      
+      this.errorMessage = '';
+      this.data = [];
       const file = document.getElementById('selectFiles').files[0];
-      // const file = ev.target.files[0];
       const reader = new FileReader();
 
       reader.onload = e => {
-        Object.keys(JSON.parse(e.target.result)).forEach(key => {
-          this.users.push({
-            key: key,
-            value: JSON.parse(e.target.result)[key],
+          Object.keys(JSON.parse(e.target.result)).forEach(key => {
+            this.data.push({
+              key: key,
+              value: JSON.parse(e.target.result)[key],
+            });
           });
-        });
-
       }
 
       reader.readAsText(file);
     },
-    testGet() {
-      console.log(this.users);
-      // export
-    },
+    getNewJSONValue() {
+      var jsonStr = '{';
+      var l = this.data.length;
+      var i = 0;
+      this.data.forEach((value, index) => {
+        i++;
+        jsonStr += '"'+this.data[index].key+'":'
+            + '"' + this.data[index].value + '"';
+
+        if (l !== i) {
+          jsonStr += ',';
+        }
+      });
+
+      jsonStr += '}';
+
+      downloadjs(jsonStr, "apidata.json", "text/plain");
+    }
   }
 };
 </script>
